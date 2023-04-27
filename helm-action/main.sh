6452590840e45076131eb3e33e2a819390cfab34
Julio Perez Abreu <jcesarpabreu@gmail.com>
2023-04-27 09:57:07 -0400

    a few more things

--- helm-action/main.sh
+++ helm-action/main.sh
@@ -1,35 +1,72 @@
 #!/bin/bash
 COMMAND=$1
-ATOMIC_TIMEOUT=$2
+TIMEOUT_IN_MINS=$2
 REPOSITORY_NAME=$3
-CHART_DIRECTORY=$4
-ENV=$5
-RELEASE_NAME=$6
-NAMESPACE=$7
-IMAGE_TAG=$8
-DRY_RUN_OPTION=$9
+HELM_DIR=$4
+VALUES_FILE=$5
+ENV=$6
+RELEASE_NAME=$7
+NAMESPACE=$8
+IMAGE_TAG=$9
+DRY_RUN_OPTION=${10}
+IS_DEBUG=${11}
+CREATE_NAMESPACE_OPTION=${12}
+SET_FLAG=${13}
+SET_STRING_FLAG=${14}
 
+CHART_DIRECTORY="${HELM_DIR}/${REPOSITORY_NAME}"
 
-if [ "${DRY_RUN_OPTION}" == "true" ]; then
-  DRY_RUN_OPTION="--dry-run"
-else
-  DRY_RUN_OPTION=""
+DRY_RUN_OPTION=$([ "${DRY_RUN_OPTION}" == "true" ] && echo "--dry-run" || echo "")
+IS_DEBUG=$([ "${IS_DEBUG}" == "true" ] && echo "--debug" || echo "")
+CREATE_NAMESPACE_OPTION=$([ "${CREATE_NAMESPACE_OPTION}" == "true" ] && echo "--create-namespace" || echo "")
+
+# Prepare --set and --set-string flags
+SET_FLAG_VALUES=""
+if [ -n "${SET_FLAG}" ]; then
+  IFS=',' read -ra SET_ENTRIES <<< "${SET_FLAG}"
+  for ENTRY in "${SET_ENTRIES[@]}"; do
+    SET_FLAG_VALUES+="--set ${ENTRY} "
+  done
+fi
+
+SET_STRING_FLAG_VALUES=""
+if [ -n "${SET_STRING_FLAG}" ]; then
+  IFS=',' read -ra SET_STRING_ENTRIES <<< "${SET_STRING_FLAG}"
+  for ENTRY in "${SET_STRING_ENTRIES[@]}"; do
+    SET_STRING_FLAG_VALUES+="--set-string ${ENTRY} "
+  done
 fi
 
 case "${COMMAND}" in
   "dependency_update")
-    echo "helm dependency update "${CHART_DIRECTORY}" ${DRY_RUN_OPTION}"
+    helm dependency update "${CHART_DIRECTORY}" ${DRY_RUN_OPTION} ${IS_DEBUG} || exit 1
     ;;
   "lint")
-    echo "helm lint --values "${CHART_DIRECTORY}/values-${ENV}.yaml" "${CHART_DIRECTORY}" ${DRY_RUN_OPTION}"
+    helm lint --values "${CHART_DIRECTORY}/${VALUES_FILE}" ${CHART_DIRECTORY} ${DRY_RUN_OPTION} ${IS_DEBUG} || exit 1
     ;;
-  "upgrade")
-    echo helm upgrade --install --atomic --timeout "${ATOMIC_TIMEOUT}" \
-      --values "${CHART_DIRECTORY}/values-${ENV}.yaml" \
+  "install")
+    helm install --timeout "${TIMEOUT_IN_MINS}m" \
+      --values "${CHART_DIRECTORY}/${VALUES_FILE}" \
+      ${SET_STRING_FLAG_VALUES} \
+      ${SET_FLAG_VALUES} \
       --set-string image.tag="${IMAGE_TAG}" \
       --set-string env.ENV="${ENV}" \
       "${RELEASE_NAME}" \
-      --namespace="${NAMESPACE}" "${CHART_DIRECTORY}/${REPOSITORY_NAME}" ${DRY_RUN_OPTION}
+      --namespace="${NAMESPACE}" \
+      "${CHART_DIRECTORY}" \
+      ${DRY_RUN_OPTION} ${IS_DEBUG} ${CREATE_NAMESPACE_OPTION} || exit 1
+    ;;
+  "upgrade")
+    helm upgrade --install --atomic --timeout "${TIMEOUT_IN_MINS}m" \
+      --values "${CHART_DIRECTORY}/${VALUES_FILE}" \
+      ${SET_STRING_FLAG_VALUES} \
+      ${SET_FLAG_VALUES} \
+      --set-string image.tag="${IMAGE_TAG}" \
+      --set-string env.ENV="${ENV}" \
+      "${RELEASE_NAME}" \
+      --namespace="${NAMESPACE}" \
+      "${CHART_DIRECTORY}" \
+      ${DRY_RUN_OPTION} ${IS_DEBUG} ${CREATE_NAMESPACE_OPTION} || exit 1
     ;;
   *)
     echo "Invalid command: ${COMMAND}"
